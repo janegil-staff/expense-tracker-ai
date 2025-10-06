8a12295bdd50ce31847a8cda216f94a3059e2e56
Jan Egil Staff Øvretveit <janstovr@Jan-sin-Air.lan>
2025-10-06 14:29:17 +0200

    Complete Navbar components and geust, about and and contact pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "@/contexts/ThemeContext";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,10 +31,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider>
-          <Navbar />
-          {children}
-        </ClerkProvider>
+        <ThemeProvider>
+          <ClerkProvider>
+            <Navbar />
+            {children}
+            <Footer />
+          </ClerkProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
